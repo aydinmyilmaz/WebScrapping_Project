f745a99db2184fb87f8bde3f87700fea8c0a5e64
I INCE <iince98@gmail.com>
2018-11-23 14:43:25 +0100

    Getting 10 pages
    Each page has 50 house info. So with "for loop", we get 10 pages

--- Web Scrapping Hurriyet Emlak_v1.py
+++ Web Scrapping Hurriyet Emlak_v1.py
@@ -1,37 +1,7 @@
-
-# coding: utf-8
-
-# In[140]:
-
-
+import pandas as pd
 import requests
-r = requests.get("https://www.hurriyetemlak.com/konut-satilik/villa/listeleme?pageSize=100")
-
 from bs4 import BeautifulSoup
-soup = BeautifulSoup(r.text,'html.parser')
-results = soup.find_all("a", attrs={'class':'overlay-link'})
 
-
-
-
-# In[143]:
-
-
-results[0:2]
-
-
-# In[150]:
-
-
-import requests
-r = requests.get("https://www.hurriyetemlak.com/konut-satilik/villa/listeleme?pageSize=100")
-
-from bs4 import BeautifulSoup
-soup = BeautifulSoup(r.text,'html.parser')
-results = soup.find_all("a", attrs={'class':'overlay-link'})
-
-
-from bs4 import BeautifulSoup
 price = []
 date = []
 area = []
@@ -40,24 +10,28 @@ room = []
 seller = []
 adres = []
 title = []
-for tag in results :
-    price.append(tag.get('data-price'))
-    date.append(tag.get('data-date'))
-    area.append(tag.get('data-meter'))
-    owner.append(tag.get('data-owner'))
-    room.append(tag.get('data-room'))
-    seller.append(tag.get('data-seller-type'))
-    adres.append(tag.get('href'))
-    title.append(tag.get('title'))
 
-records={"title": title, "price": price, "date":date, "area-m2": area, "owner":owner, "room": room, "seller": seller, "adres":adres, }
+#Getting related info for 10 pages, each consists 50 house 
+for i in range(10):
+    r = requests.get("https://www.hurriyetemlak.com/konut-satilik/villa/listeleme?pageSize=50&page="+str(i))
+    soup = BeautifulSoup(r.text,'html.parser')
+    
+    #Info related to house exists in "a" tag
+    results = soup.find_all("a", attrs={'class':'overlay-link'})
+    for tag in results :
+        price.append(tag.get('data-price'))
+        date.append(tag.get('data-date'))
+        area.append(tag.get('data-meter'))
+        owner.append(tag.get('data-owner'))
+        room.append(tag.get('data-room'))
+        seller.append(tag.get('data-seller-type'))
+        adres.append(tag.get('href'))
+        title.append(tag.get('title'))
 
-import pandas as pd
+    #creating a dictionary with title (keys) and the house info (values)
+    records={"title": title, "price": price, "date":date, "area-m2": area, "owner":owner, "room": room, "seller": seller, "adres":adres, }
 
 df = pd.DataFrame(records)
-df.head(15)
+print (df)
 
 df.to_csv("hurriyet.txt")
-
-df.head(15)
-
